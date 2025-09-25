ca73a6a017a9a495517c30ee0c65eaf495693b02
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-09-25 14:09:19 +0200

    added enemy flip

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -6,11 +6,13 @@ public class EnemyChase : MonoBehaviour
     public float moveSpeed = 3f;
     Transform player;
     Rigidbody2D rb;
+    SpriteRenderer sr;
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player")?.transform;
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>(); // or GetComponent<SpriteRenderer>()
     }
 
     void FixedUpdate()
@@ -19,5 +21,12 @@ public class EnemyChase : MonoBehaviour
 
         Vector2 dir = ((Vector2)player.position - rb.position).normalized;
         rb.MovePosition(rb.position + dir * moveSpeed * Time.fixedDeltaTime);
+
+        // Flip horizontally if needed
+        if (sr)
+        {
+            // Facing left by default, so flip when player is to the RIGHT
+            sr.flipX = (player.position.x > transform.position.x);
+        }
     }
 }
