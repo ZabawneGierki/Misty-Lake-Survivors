608586b130d88ee32d5246ddbb3a270871832463
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-09-29 18:42:00 +0200

    making power ups

---
+++ Assets/Scripts/WeaponSizeBoost.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BulletHeaven/PowerUps/Weapon Size Boost")]
+public class WeaponSizeBoost : PowerUpEffect
+{
+    [Tooltip("Extra size per level, e.g. 0.1 = +10%")]
+    public float sizeBonusPerLevel = 0.1f;
+
+    public override void Apply(PlayerInventory inventory, int newLevel)
+    {
+        // multiplier = 1 + (bonus * level)
+        float multiplier = 1f + sizeBonusPerLevel * newLevel;
+
+        foreach (var w in inventory.weapons)
+        {
+            // WeaponBase already has sizeMultiplier from earlier design
+            if (w.instance)
+            {
+                var baseScript = w.instance.GetComponent<WeaponBase>();
+                if (baseScript != null)
+                    baseScript.sizeMultiplier = multiplier;
+            }
+        }
+    }
+}
