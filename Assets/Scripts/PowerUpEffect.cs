cbcb7bf65eca61316350de17da5d40ab2e13c78c
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-09-29 18:39:40 +0200

    making weapon icons

--- Assets/Scripts/PowerUpEffect.cs
+++ Assets/Scripts/PowerUpEffect.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUpEffect : MonoBehaviour
+public abstract class PowerUpEffect : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string powerName;
+    public Sprite icon;
+    public string description;
+    public int maxLevel = 5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Called when this power-up is picked or leveled up.
+    public abstract void Apply(PlayerInventory inventory, int newLevel);
 }
