76b47e29947a3dda0324724695b7dbb384065c7d
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-03 18:41:55 +0200

    adding laser weapon

--- Assets/Scripts/Weapons/Weapons Proper/Laser.cs
+++ Assets/Scripts/Weapons/Weapons Proper/Laser.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Laser : WeaponBase
 {
+    public float damagePerTick = 5f;
+    public float tickRate = 0.2f; // damage every 0.2s
+    public float lifetime = 1f;   // how long beam exists
     protected override void Fire()
     {
         throw new System.NotImplementedException();
