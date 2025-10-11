041e1b5acbb38cd8ae0badefd8a7f4c4dd137d92
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-11 18:54:15 +0200

    added character portraits

--- Assets/Scripts/CharacterButtonLoader.cs
+++ Assets/Scripts/CharacterButtonLoader.cs
@@ -1,30 +1,33 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
 [System.Serializable]
-public class  CharacterData
+public class CharacterData
 {
     string fullName;
     CharacterNames name;
     Sprite characterPortrait;
 
-        
+
 }
 public class CharacterButtonLoader : MonoBehaviour
 {
     [SerializeField]
-     List<CharacterData> characterDataList;
+    List<CharacterData> characterDataList;
+    [SerializeField] GameObject characterButtonPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach(CharacterData character in characterDataList)
+        {
+            Debug.Log(character);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
