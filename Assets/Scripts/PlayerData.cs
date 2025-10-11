ac10658b9282d79f7e23dab34b5d0cc4e72002b9
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-11 18:37:06 +0200

    added player names

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum CharacterNames
+{
+        Reimu,
+        Marisa,
+        Cirno,
+        Daiyousei,
+}
+public static class PlayerData  
+{
+     public static CharacterNames selectedcharacterName = CharacterNames.Reimu;
+}
