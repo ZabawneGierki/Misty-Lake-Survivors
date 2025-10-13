14ef800a1dac2d33bdfa7376b8c111f9b44ead0d
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-13 16:10:19 +0200

    creating toggle button

--- Assets/ToggleAnimation.cs
+++ Assets/ToggleAnimation.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Image BackgroundImage;
+    private void Awake()
     {
-        
+        BackgroundImage = GetComponentInParent<Image>();
+        Debug.Log(BackgroundImage.name);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void AnimateToggle(bool isOn)
     {
-        
+        Animator animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("isOn", isOn);
+        }
     }
 }
