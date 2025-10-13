829746f1761a4e293345baf667ecb5a88405bccc
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-13 17:31:42 +0200

    imported images

---
+++ Assets/Scripts/Menu/LevelButtonLoader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+
+public class LevelData
+{
+    public string levelName;
+    public int levelIndex;
+    public Sprite levelThumbnail;
+}
+public class LevelButtonLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
