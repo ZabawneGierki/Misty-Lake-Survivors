1ab485525c6a292e5441ccf0a2d26d379dde3faf
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-15 22:20:49 +0200

    added return button

--- Assets/Scripts/Menu/ScreenManager.cs
+++ Assets/Scripts/Menu/ScreenManager.cs
@@ -13,14 +13,29 @@ public enum ScreenName
 [System.Serializable]
 public class  Screen
 {
-    GameObject screenRef;
-    ScreenName screenName;
+    public GameObject screenRef;
+    public ScreenName screenName;
 }
 public class ScreenManager : MonoBehaviour
 {
     private static ScreenManager instance;
     public static ScreenManager Instance { get { return instance; } }
     [SerializeField] List<Screen> screens;
+
+    public void ShowScreen(ScreenName screenName)
+    {
+        foreach (var screen in screens)
+        {
+            if (screen.screenName == screenName)
+            {
+                screen.screenRef.SetActive(true);
+            }
+            else
+            {
+                screen.screenRef.SetActive(false);
+            }
+        }
+    }
 
     private void Awake()
     {
