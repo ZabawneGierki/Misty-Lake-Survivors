8b7f3106b7b0fab8754008f320e441fd057441ee
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2025-10-17 16:10:52 +0200

    added save data enum

--- Assets/Scripts/Menu/SaveManager.cs
+++ Assets/Scripts/Menu/SaveManager.cs
@@ -1,18 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SaveManager 
+[System.Serializable]
+public enum PermanentUpgradeName
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    MaxHealth,
+    Damage,
+    Speed,
+    FireRate,
+    ShieldCapacity,
+    HealthRegen
+}
+public static class SaveManager
+{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
